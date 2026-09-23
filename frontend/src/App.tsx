6ee784147020a2ba545c0fcba79e52aede497ba9
Arkturiuso <tagaykirill83@gmail.com>
2026-09-23 10:23:04 +0500

    feat: add auth page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,25 @@
-export const App = () => <h1>Hello world</h1>
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ROUTES } from './routes';
+
+const LoginPage = lazy(() => import('./pages/Authorization/index'));
+
+const App = () => (
+  <BrowserRouter>
+    <Suspense>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={ROUTES.AUTH.LOGIN} replace />}
+        />
+        <Route path={ROUTES.AUTH.LOGIN} element={<LoginPage />} />
+        <Route
+          path="*"
+          element={<Navigate to={ROUTES.AUTH.LOGIN} replace />}
+        />
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
+);
+
+export default App;
